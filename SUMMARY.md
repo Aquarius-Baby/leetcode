cb277a48f4017b749c6df7d41776306ff37195e2
Aquarius-Baby <chenmengyan93@163.com>
2019-12-12 16:22:59 +0800

    T1

--- SUMMARY.md
+++ SUMMARY.md
@@ -1,3 +1,4 @@
 # Summary
 
 - [介绍](README.md)
+[001  两数之和](src/part1/leetcode001.java)
---
+++ src/util/TreeNode.java
@@ -0,0 +1,63 @@
+package util;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+    public static TreeNode getTreeNode(int[] temp) {
+        if (temp == null) {
+            return null;
+        }
+        TreeNode root = new TreeNode(temp[0]);
+        help(temp, 0, root);
+        return root;
+    }
+
+    private static void help(int[] temp, int index, TreeNode root) {
+        if (index >= temp.length) {
+            return;
+        }
+        int length = temp.length;
+        if (2 * index + 1 < length) {
+            int leftIndex = 2 * index + 1;
+            if (temp[leftIndex] == Integer.MIN_VALUE) {
+                return;
+            }
+            root.left = new TreeNode(temp[leftIndex]);
+            help(temp, leftIndex, root.left);
+        }
+        if (2 * index + 2 < length) {
+            int rightIndex = 2 * index + 2;
+            if (temp[rightIndex] == Integer.MIN_VALUE) {
+                return;
+            }
+            root.right = new TreeNode(temp[rightIndex]);
+            help(temp, rightIndex, root.right);
+        }
+    }
+
+    /**
+     * 中序遍历
+     */
+
+    private  static void mid(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.println(root.val);
+        mid(root.left);
+        mid(root.right);
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, Integer.MIN_VALUE, 4, 5, 6};
+        TreeNode root = TreeNode.getTreeNode(a);
+        TreeNode.mid(root);
+    }
+}
